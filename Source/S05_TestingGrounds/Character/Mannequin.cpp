00dafe424389b08a072b7c39710f4ba70815d8a3
Marco Maida <00marcomaida00@gmail.com>
2017-09-06 12:48:10 +0200

    Some fixes to gitignore, added firing binding

--- Source/S05_TestingGrounds/Character/Mannequin.cpp
+++ Source/S05_TestingGrounds/Character/Mannequin.cpp
@@ -38,6 +38,11 @@ void AMannequin::BeginPlay()
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
 	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+
+	if (InputComponent != NULL)
+	{
+		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::Fire);
+	}
 }
 
 // Called every frame
